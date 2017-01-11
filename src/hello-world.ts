e2ade71e954dc567e70f594dda2760e51c1048cd
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-11 09:29:38 -0300

    Arrow Function

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,3 +1,18 @@
 const message = "Hello World!";
 
-console.log(message);
+const sayHello = message => console.log(message);
+
+sayHello(message);
+
+function Person(name) {
+    this.name = name;
+
+    this.sayHi = function() {
+        setTimeout(() => {
+            console.log("Hello my name is ", this.name);
+        })
+    }
+}
+
+const person = new Person("Max");
+person.sayHi();
