fc4a2cc4d3cf7e21d6a55820e65381f69a78f55c
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-11 22:28:42 -0300

    Types Examples

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,41 @@
-console.log("Types!");
+console.log('Types');
+
+let message:string = "Message";
+
+let count:number = 3;
+
+let messageRed:boolean =true;
+
+type hasName = {
+    firstName?:string, //Conditional
+    lastName:string
+}
+
+type hasAddress = {
+    streetName:string
+}
+
+type Person = {
+    name: hasName,
+    address: hasAddress
+}
+
+let person: hasName = {
+    firstName: "Kobe",
+    lastName: "Bryant"
+}
+
+person.address = ''; //Property not Defined error
+
+let person2: hasName = {
+    lastName: "Smith";
+}
+
+let person3: Person = {
+    name: {
+        lastName: "Miller"
+    },
+    address: {
+        streetName: "Streen Name"
+    }
+}
