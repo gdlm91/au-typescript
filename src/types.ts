345af55a6871df33da434af8d72ce71a5fc00c01
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-11 23:36:26 -0300

    @types for Third Parties

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,7 @@
-// Interfaces to Define Properties for Objects
-interface HasName {
-    firstName: string,
-    lastName: string
-}
-interface HasAddress {
-    address: string
-}
+import * as _ from 'lodash';
 
-// Types for Aliases/Unions
-type Player = (HasName & HasAddress) | null;
+const colors = ["Red", "Greem", "Blue"];
 
-const player1: Player = {
-    firstName: "Magic",
-    lastName: "Johnson",
-    address: "Staples"
-}
+const firstColor = _.first(colors);
 
-const player2: Player = null;
-
-let myVar: any;
-let counter: number = 0;
-
-function anyFunction(anyvar) { //Implicit Any Assing. (Go to tsconfig for configuration)
-
-}
-
-myVar = '';
-myVar = 1;
-myVar = [];
-myVar = null;
-
-counter = myVar; // Can assing any to other types!!!
+console.log(firstColor);
