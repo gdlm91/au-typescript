2d8caa598d8a370d7369415b82a2cb97db3f077f
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-16 08:59:23 -0300

    onSuccess on apiGetAllCourses

--- src/server/api/apiGetAllCourses.ts
+++ src/server/api/apiGetAllCourses.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import { findAllCourses } from "../queries/findAllCourses";
+import { onSuccess } from "./onSuccess";
 import { onError } from "./onError";
 
 export function apiGetAllCourses(req: Request, res: Response) {
     findAllCourses()
-        .then(results => {
-            res.status(200).json({ results });
-        })
+        .then(results => onSuccess(res, results))
         .catch(err => onError(res, "Find All Courses Failed", err));
 }
