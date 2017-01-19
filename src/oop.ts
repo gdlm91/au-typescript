32fc27460c42160ecc229d68e97f68c811b2f59d
Gerardo Leal <gerard.leal91@gmail.com>
2017-01-18 22:45:34 -0300

    OOP: Classes, Methods and Properties

---
+++ src/oop.ts
@@ -0,0 +1,48 @@
+enum Editor {
+    MARVEL,
+    DC
+}
+
+abstract class SuperHero {
+
+    private static readonly LABEL = 'Super Hero:';
+
+    constructor(
+        public name: string,
+        protected _editor: Editor,
+        protected createYear: number
+    ) { }
+
+    abstract createMessage(): string;
+
+    static sayHi(hero: SuperHero): string {
+        return `${SuperHero.LABEL} ${hero.name} sais Hi!`;
+    }
+}
+
+class FlyingHero extends SuperHero {
+    fly(message: string) {
+        console.log(`${this.name} is Flying and said: ${message}`);
+    }
+    
+    createMessage() {
+        return `Super Hero: ${this.name} ${this.editor}, ${this.createYear}`;
+    }
+
+    get editor(): string {
+        return Editor[this._editor];
+    }
+
+    set editor(editorName: string) {
+        this._editor = (<any>Editor)[editorName];
+    }
+}
+
+const superman = new FlyingHero("Superman", Editor.MARVEL, 1938);
+superman.fly("Up and Away!");
+superman.editor = "DC";
+console.log(superman.createMessage());
+
+const greenLantern = new FlyingHero("Green Lantern", Editor.DC, 1959);
+console.log(greenLantern.createMessage());
+console.log(SuperHero.sayHi(greenLantern));
